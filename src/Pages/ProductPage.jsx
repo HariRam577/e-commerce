d56ffbb87ec79228c02a3e51e7e1b5b5cbb97921
Hariram <hariram007cm@gmail.com>
2025-11-03 11:21:25 +0530

    corrections

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -33,7 +33,7 @@ const ProductPage = () => {
           {data.map((product) => (
             <div
               key={product.id}
-              className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
+              className="flex flex-col border border-gray-300 rounded-lg p-4 shadow hover:shadow-lg transition-shadow duration-300"
             >
               <img
                 src={product.image}
