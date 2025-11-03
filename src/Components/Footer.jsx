4dc427eb577a669bbe4d5a33e3cb8ef64158d5c5
Hariram <hariram007cm@gmail.com>
2025-11-03 13:20:52 +0530

    footer theme changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <footer className="bg-gray-900 text-white mt-20 py-12">
+      <footer className="bg-theme dark:bg-theme-dark text-black dark:text-white  mt-20 py-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
             <div>
